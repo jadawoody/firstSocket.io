a98a2cbc52ce4c6ef3eaec1514ffd8ff71a97735
Jada Woody <JadaWoody@ENCMBP01.local>
2020-09-03 15:23:40 -0400

    try again

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ io.on('connection', (socket) => {
       io.emit('chat message', msg);
     });
   });
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
